a890f821af2e428a75e8c37e380faaa7c2558364
Archana Pujahari <archanapujahari26@gmail.com>
2026-04-16 17:19:02 +0530

    Import export_to_csv function in main.py

--- expense-tracker/main.py
+++ expense-tracker/main.py
@@ -1,5 +1,6 @@
 from database import connect, add_expense, view_expenses, delete_expense, monthly_summary
 from analysis import plot_monthly_expenses
+from export import export_to_csv
 
 def menu():
     print("\n===== Expense Tracker =====")
